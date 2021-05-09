5dce5a847d0885bb38dcebad5f75926cef347305
Vultwo <n.kroesen@outlook.com>
2021-05-09 12:34:54 +0200

    Added denormalize, fixed normalize

--- ta_py/ta.py
+++ ta_py/ta.py
@@ -36,10 +36,15 @@ def kmeans(data, clusters):
             if centers[x] != old[x]: changed = True;
     return means;
 def normalize(data, marg=0):
-    max = max(data)*(1+marg); min = min(data)*(1+marg); norm = [];
+    max = max(data)*(1+marg); min = min(data)*(1-marg); norm = [];
     for i in range(0, len(data)):
         norm.append(1-(max-data[i])/(max-min));
     return norm;
+def denormalize(data, norm, marg=0):
+    max = max(data)*(1+marg); min = min(data)*(1-marg); dnorm = [];
+    for i in range(0, len(norm)):
+        dnorm.append(min+norm[i]*(max-min));
+    return dnorm;
 def mad(data, l=0):
     l = l if l > 0 else len(data); med = [];
     for i in range(l, len(data)+1):
