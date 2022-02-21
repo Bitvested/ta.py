44773984c1b63b289807eb0b70d78d84006fc4f5
Vultwo <n.kroesen@outlook.com>
2022-02-21 16:41:54 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='ta_py',
-     version='1.3.0',
+     version='1.3.1',
      author="Nino Kroesen",
      author_email="ninokroesen@gmail.com",
      description="ta.py is a Python package for dealing with financial technical analysis",
